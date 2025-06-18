4bff556a03db2efca6b141e2fa51bc25d462b225
Pascal Fliednr <fliednerpascal@gmail.com>
2025-06-18 20:32:01 +0200

    Implemented the Header , Navbar and the About me Section

---
+++ src/app/main-page/main-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillSetComponent } from './skill-set/skill-set.component';
+
+@Component({
+    selector: 'app-main-page',
+    imports: [HeaderComponent, HeroComponent, AboutMeComponent, SkillSetComponent],
+    templateUrl: './main-page.component.html',
+    styleUrl: './main-page.component.scss',
+})
+export class MainPageComponent {}
