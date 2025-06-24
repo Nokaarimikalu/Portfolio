31ce619d34b6131c3d3a5bcc7e6fa02bc07a3fa8
Pascal Fliednr <fliednerpascal@gmail.com>
2025-06-24 21:01:53 +0200

    Skillset

--- src/app/main-page/skill-set/small-icons/small-icons.component.ts
+++ src/app/main-page/skill-set/small-icons/small-icons.component.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
 
+interface Icon {
+    url: string;
+    name: string;
+}
+
 @Component({
-  selector: 'app-small-icons',
-  imports: [],
-  templateUrl: './small-icons.component.html',
-  styleUrl: './small-icons.component.scss'
+    selector: 'app-small-icons',
+    imports: [],
+    templateUrl: './small-icons.component.html',
+    styleUrl: './small-icons.component.scss',
 })
 export class SmallIconsComponent {
-
+    icons: Icon[] = [
+        {
+            url: '/assets/image/skill-set/small-icons/HTML.png',
+            name: 'HTML' 
+        },
+        {
+            url: '/assets/image/skill-set/small-icons/CSS.png', 
+            name: 'CSS' 
+        },
+        {
+            url: '/assets/image/skill-set/small-icons/JavaScript.png',
+            name: 'JavaScript',
+        },
+        {
+            url: '/assets/image/skill-set/small-icons/TypeScript.png',
+            name: 'TypeScript',
+        },
+        {
+            url: '/assets/image/skill-set/small-icons/Angular.png',
+            name: 'Angular',
+        },
+        {
+            url: '/assets/image/skill-set/small-icons/Firebase.png',
+            name: 'Firebase',
+        },
+        { 
+            url: '/assets/image/skill-set/small-icons/Git.png', 
+            name: 'Git' 
+        },
+        { 
+            url: '/assets/image/skill-set/small-icons/API.png', 
+            name: 'API' 
+        },
+    ];
 }
