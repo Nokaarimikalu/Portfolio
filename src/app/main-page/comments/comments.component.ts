27b40e8dbbb361c6ff2879c0627132436dfaf1cb
Pascal Fliednr <fliednerpascal@gmail.com>
2025-06-29 15:09:33 +0200

    comments myProjects Skillset and about me responsive gemacht

--- src/app/main-page/comments/comments.component.ts
+++ src/app/main-page/comments/comments.component.ts
@@ -16,21 +16,21 @@ interface Comments {
 export class CommentsComponent {
     CommentOne: Comments = 
         {
-            text: "man kann sich unterhalten",
+            text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi, nostrum molestiae. Suscipit eius, dignissimos earum incidunt dolorem nisi cupiditate ipsum laudantium ratione eaque iste iure iusto debitis corporis voluptas culpa!",
             name: "Keven Gebauer",
             work: "Frontend Developer",
             profileLink: "#"
         }
     CommentTwo: Comments = 
         {
-            text: "Brutaler Kerl",
+            text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi, nostrum molestiae. Suscipit eius, dignissimos earum incidunt dolorem nisi cupiditate ipsum laudantium ratione eaque iste iure iusto debitis corporis voluptas culpa!",
             name: "Essma",
             work: "Frontend Developer",
             profileLink: "#"
         }
     CommentThree: Comments = 
         {
-            text: "Klasse kerl!",
+            text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi, nostrum molestiae. Suscipit eius, dignissimos earum incidunt dolorem nisi cupiditate ipsum laudantium ratione eaque iste iure iusto debitis corporis voluptas culpa!!",
             name: "WIllstma ",
             work: "Frontend Developer",
             profileLink: "#"
