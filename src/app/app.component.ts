9d92493f82a9a9d5d91ccf313e416724a508c3f9
Pascal Fliednr <fliednerpascal@gmail.com>
2025-06-29 19:18:15 +0200

    umstrukturierung des Projektes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainPageComponent],
+  imports: [RouterOutlet, HeaderComponent , MainPageComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillSetComponent } from './skill-set/skill-set.component';
 import { MyProjectsComponent } from './my-projects/my-projects.component';
 import { CommentsComponent } from './comments/comments.component';
-import { FooterComponent } from './footer/footer.component';
+
 
 @Component({
     selector: 'app-main-page',
-    imports: [HeaderComponent, HeroComponent, AboutMeComponent, SkillSetComponent, MyProjectsComponent, CommentsComponent, FooterComponent],
+    imports: [ HeroComponent, AboutMeComponent, SkillSetComponent, MyProjectsComponent, CommentsComponent],
     templateUrl: './main-page.component.html',
     styleUrl: './main-page.component.scss',
 })
